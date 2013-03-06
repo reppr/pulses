f6045cbbb09967eaa3aaffdc6d450c1f149d508e
Robert Epprecht <epprecht@solnet.ch>
2013-03-06 09:39:37 +0100

    Repo merge script: take paths and mangle them.

--- misc/scripts/git-merge-repositories-subtree-merge-no-clone.sh
+++ misc/scripts/git-merge-repositories-subtree-merge-no-clone.sh
@@ -1,8 +1,8 @@
-#! /bin/sh
+#! /bin/bash
 # '  ' indentation is for inclusion in git commit messages,
 # as lines starting with '#' (like this line) get removed...
 
-  #! /bin/sh
+  #! /bin/bash
 
   # ****************************************************************
   # Script used to merge other source directories into an existing one
@@ -10,22 +10,54 @@
   # '  ' indented to protect #comments when included in commit message.
   # ****************************************************************
   commandline="$*"
-
-  DEST_GIT="/home/dada/PULSES_i5/pulses"
-
-#  OTHER_GIT="/home/dada/ARDUINO-GIT2/softboard"
-  OTHER_GIT="/home/dada/arduino-GIT1"
-
-  DEST_COPY_DIR="/tmp/GIT-new.$$"
-  OTHER_TMP="/tmp/GIT-OTHER.$$"
-
-  DEST_PAREN="${DEST_GIT%/*}"
-  DEST_SUBDIR="${DEST_GIT##*/}"
-  OTHER_SUBDIR="${OTHER_GIT##*/}"
+  # ****************************************************************
 
 
   # ****************************************************************
-  # Use colours to make output more readable
+  mangle_path() {		# X="$(mangle_path "$1")"
+      [ -z "${1}" ] && {
+	  # echo "${0##*/}  ${red}ERROR: empty path${treset}!"
+	  echo "${0##*/}  $(tput setaf 1)ERROR: empty path$(tput sgr0)!"
+          # usage
+	  exit 1
+      }
+      NEW_PATH="${1}"
+      NEW_PATH="${NEW_PATH#./}"			# no starting ./
+      [ "${NEW_PATH}" == '.' ] && NEW_PATH="$(pwd)"	# no '.'
+      [ "${NEW_PATH:0:1}" != "/" ] && NEW_PATH="$(pwd)/${NEW_PATH}"
+      if [ "${NEW_PATH}" != '/' ] ; then		# accept '/'
+	  NEW_PATH="${NEW_PATH%/}"			# no trailing /
+      fi
+      echo "${NEW_PATH}"
+  }
+
+
+  # ****************************************************************
+  #MAIN_GIT="/home/dada/PULSES_i5/pulses"
+  MAIN_GIT="$(mangle_path "${1}")"
+  MAIN_PAREN="${MAIN_GIT%/*}"
+  MAIN_SUBDIR="${MAIN_GIT##*/}"
+
+  #  OTHER_GIT_SRC="/home/dada/ARDUINO-GIT2/softboard"
+  #  OTHER_GIT_SRC="/home/dada/arduino-GIT1"
+  OTHER_GIT_SRC="$(mangle_path "${2}")"
+  OTHER_SUBDIR="${OTHER_GIT_SRC##*/}"
+
+  shift 2
+
+  NEW_MAIN="/tmp/newGIT_${OTHER_SUBDIR}.$$"
+  OTHER_TMP="/tmp/OTHER_CLONE.$$"
+
+  # echo "MAIN_GIT	$MAIN_GIT"
+  # echo "MAIN_PAREN	$MAIN_PAREN"
+  # echo "MAIN_SUBDIR	$MAIN_SUBDIR"
+  # echo "OTHER_GIT_SRC	$OTHER_GIT_SRC"
+  # echo "OTHER_SUBDIR	$OTHER_SUBDIR"
+  # echo "NEW_MAIN	$NEW_MAIN"
+
+
+  # ****************************************************************
+  # Use colours to make output more readable.
   # Get colour codes:
   black=$(tput setaf 0)
   red=$(tput setaf 1)
@@ -48,6 +80,7 @@
   # echo "${white}white"
   # echo "${treset}treset"
 
+
   # ****************************************************************
   # Debugging:
   # Personal git error return codes:
@@ -66,9 +99,9 @@
   # ****************************************************************
   echo "${cyan}${0##*/}"
   echo ${yellow}
-  echo "Testing main git repo ${DEST_GIT}${treset}"
-  cd ${DEST_GIT}		  || exit 1
-  cd ${DEST_PAREN}/${DEST_SUBDIR} || exit 1	# should be same ;)
+  echo "Testing main git repo ${MAIN_GIT}${treset}"
+  cd ${MAIN_GIT}		  || exit 1
+  cd ${MAIN_PAREN}/${MAIN_SUBDIR} || exit 1	# should be same ;)
   git status || {
       echo ${red}${?}${treset}
       echo "  'git status' ${red}FAILED.${treset}"
@@ -76,9 +109,9 @@
   }
 
   echo ${yellow}
-  echo "Testing other git repo ${OTHER_GIT}${treset}"
-  cd $OTHER_GIT			  	|| exit 1
-  cd ${OTHER_GIT%/*}/${OTHER_SUBDIR}	|| exit 1	# same ;)
+  echo "Testing other git repo ${OTHER_GIT_SRC}${treset}"
+  cd $OTHER_GIT_SRC			  	|| exit 1
+  cd ${OTHER_GIT_SRC%/*}/${OTHER_SUBDIR}	|| exit 1	# same ;)
   git status || {
       echo ${red}${?}${treset}
       echo "  'git status' ${red}FAILED.${treset}"
@@ -88,21 +121,20 @@
   
   # ****************************************************************
   # Start all over again...
-  rm -rf $OTHER_TMP $DEST_COPY_DIR  # obsolete as long as we use $$
+  rm -rf $OTHER_TMP $NEW_MAIN  # obsolete as long as we use $$
   
   
   # ****************************************************************
-  echo "${magenta}Clone ${OTHER_GIT} to temporary location ${OTHER_TMP}${treset}"
+  echo "${magenta}Clone ${OTHER_GIT_SRC} to temporary location ${OTHER_TMP}${treset}"
   # git clones into a subdir named after the paren directory of the
   # cloned repository:
-  OTHER_SUBDIR="${OTHER_GIT##*/}"
 
   mkdir ${OTHER_TMP} || exit 1
   cd ${OTHER_TMP}    || exit 1
 
-  git clone ${OTHER_GIT} || {
+  git clone ${OTHER_GIT_SRC} || {
       echo ${red}${?}${treset}
-      echo "  'git clone ${OTHER_GIT}' ${red}FAILED.${treset}"
+      echo "  'git clone ${OTHER_GIT_SRC}' ${red}FAILED.${treset}"
       exit $gitErrClone
   }
   #pwd ; ls -Al
@@ -125,7 +157,7 @@
   echo ${cyan}
   echo "Include this script in commit message.${treset}"
 
-  git commit --allow-empty -m "Prepare merge of ${OTHER_GIT}
+  git commit --allow-empty -m "Prepare merge of ${OTHER_GIT_SRC}
   
   Preparing repository merge: Do not use.
   
@@ -177,22 +209,21 @@
   "
   read dummy
 
-  echo ${magenta}
-  #PS1='\$' bash -c'source /etc/profile'		################
-  PS1='\$' bash -i
+  PS1="${cyan}\w ${red}\$ ${magenta}"  bash --norc -i
   echo ${treset}
+  echo "(back fom sub shell)"
 
 
   # ****************************************************************
-  echo "${cyan}cp -ap $DEST_PAREN $DEST_COPY_DIR${treset}"
-  mkdir $DEST_COPY_DIR			|| exit 1
-  cd ${DEST_PAREN}			|| exit 1
-  cp -ap $DEST_SUBDIR $DEST_COPY_DIR	|| exit 1
+  echo "${cyan}cp -ap $MAIN_PAREN $NEW_MAIN${treset}"
+  mkdir $NEW_MAIN			|| exit 1
+  cd ${MAIN_PAREN}			|| exit 1
+  cp -ap $MAIN_SUBDIR $NEW_MAIN	|| exit 1
   echo "	${blue}<ENTER>${treset}"
   read dummy
 
-  echo "${cyan}Merge ${OTHER_GIT} repo into ${DEST_COPY_DIR}${treset}"
-  cd ${DEST_COPY_DIR}/${DEST_SUBDIR}	|| exit 1
+  echo "${cyan}Merge ${OTHER_GIT_SRC} repo into ${NEW_MAIN}${treset}"
+  cd ${NEW_MAIN}/${MAIN_SUBDIR}	|| exit 1
   #echo ; pwd ; ls -al ; git status
 
 
@@ -210,7 +241,7 @@
   echo "	${blue}<ENTER>${treset}"
   read dummy
 
-  git merge --no-commit -m "Merged ${OTHER_GIT}
+  git merge --no-commit -m "Merged ${OTHER_GIT_SRC}
   
   Transition: Do not use.
   
@@ -266,8 +297,8 @@
   echo
   echo "************************************************"
   echo "${0##*/}  ${cyan}done :)"
-  echo "New git repo is in ${treset}${DEST_COPY_DIR}"
-  echo "${yellow}Check and copy to ${treset}${DEST_GIT} ${yellow}by hand.${treset}"
+  echo "New git repo is in ${treset}${NEW_MAIN}"
+  echo "${yellow}Check and copy to ${treset}${MAIN_GIT} ${yellow}by hand.${treset}"
   echo "************************************************"
 
   echo ${cyan}
@@ -292,8 +323,7 @@
   "
   read dummy
 
-  echo -n ${yellow}
-  PS1='\$' bash -i
+  PS1="${cyan}\w ${red}\$ ${yellow}"  bash --norc -i
   echo ${treset}
 
   # ****************************************************************
