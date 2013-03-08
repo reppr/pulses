f513958590bab953c33c2b4e9e0972638eb33db5
Robert Epprecht <epprecht@solnet.ch>
2013-03-08 04:58:50 +0100

    Start writing library 'Pulses' based on polyphonic_oscillators.pde

---
+++ libraries/Pulses/Pulses.h
@@ -0,0 +1,50 @@
+/* **************************************************************** */
+/*
+   Pulses.h
+*/
+/* **************************************************************** */
+#ifndef PULSES_h
+#define PULSES_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef ARDUINO
+  #define STREAMTYPE	Stream
+#else
+  #include <iostream>
+
+  using namespace std;
+  ostream & Serial=cout;	// nice trick from johncc
+  #define STREAMTYPE	ostream
+#endif
+
+/* **************************************************************** */
+#ifndef USE_F_MACRO
+  // For tests and on PC:  Fake Arduino F("string") macro as noop:
+  #define F(s)	(s)
+#endif
+
+
+/* **************************************************************** */
+// Preprocessor macro logic:
+
+/* Debugging macros:		*/
+#ifdef DEBUGGING_ALL
+
+#endif
+
+
+#if defined(ARDUINO)
+  #if ARDUINO >= 100
+    #include "Arduino.h"
+  #else
+    #include "WProgram.h"
+  #endif
+#endif // ARDUINO
+
+/* **************************************************************** */
+
+
+/* **************************************************************** */
+#endif
