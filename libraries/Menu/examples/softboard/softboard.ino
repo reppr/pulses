06e892f0eca1def4c2bc8e5ab6965b8bd56d14ab
Robert Epprecht <epprecht@solnet.ch>
2016-11-16 07:28:11 +0000

    NodeMCU digital pin macros included in softboard as comment, not used yet.
    modified:   libraries/Menu/examples/softboard/softboard.ino
    modified:   libraries/Menu/examples/softboard/softboard_page.ino.HIDE
    modified:   libraries/Pulses/examples/pulses/softboard_page.ino

--- libraries/Menu/examples/softboard/softboard.ino
+++ libraries/Menu/examples/softboard/softboard.ino
@@ -83,6 +83,22 @@ int men_getchar() {
 Menu MENU(32, 1, &men_getchar, Serial);
 
 
+// NodeMCU ESP8266 board
+/*
+  // on NodeMCU ESP8266 board Arduino defines digital pins
+  static const uint8_t D0   = 16;
+  static const uint8_t D1   = 5;
+  static const uint8_t D2   = 4;
+  static const uint8_t D3   = 0;
+  static const uint8_t D4   = 2;
+  static const uint8_t D5   = 14;
+  static const uint8_t D6   = 12;
+  static const uint8_t D7   = 13;
+  static const uint8_t D8   = 15;
+  static const uint8_t D9   = 3;
+  static const uint8_t D10  = 1;
+ */
+
 /* **************************************************************** */
 /* Arduiono setup() and loop():					*/
 
@@ -145,6 +161,21 @@ void loop() {	// ARDUINO
 /* ARDUINO BOARD SPECIFIC THINGS  try to use ARDUINO MACROS: */
 #include <Boards.h>
 
+// NodeMCU ESP8266 board
+/*
+  // on NodeMCU ESP8266 board Arduino defines digital pins
+  static const uint8_t D0   = 16;
+  static const uint8_t D1   = 5;
+  static const uint8_t D2   = 4;
+  static const uint8_t D3   = 0;
+  static const uint8_t D4   = 2;
+  static const uint8_t D5   = 14;
+  static const uint8_t D6   = 12;
+  static const uint8_t D7   = 13;
+  static const uint8_t D8   = 15;
+  static const uint8_t D9   = 3;
+  static const uint8_t D10  = 1;
+ */
 
 #ifndef NUM_DIGITAL_PINS		// try harder... ?
   #warning "#define NUM_DIGITAL_PINS	// FIXME: ################"
--- libraries/Menu/examples/softboard/softboard_page.ino.HIDE
+++ libraries/Menu/examples/softboard/softboard_page.ino.HIDE
@@ -84,6 +84,22 @@
 #include <pins_arduino.h>
 
 
+// NodeMCU ESP8266 board
+/*
+  // on NodeMCU ESP8266 board Arduino defines digital pins
+  static const uint8_t D0   = 16;
+  static const uint8_t D1   = 5;
+  static const uint8_t D2   = 4;
+  static const uint8_t D3   = 0;
+  static const uint8_t D4   = 2;
+  static const uint8_t D5   = 14;
+  static const uint8_t D6   = 12;
+  static const uint8_t D7   = 13;
+  static const uint8_t D8   = 15;
+  static const uint8_t D9   = 3;
+  static const uint8_t D10  = 1;
+ */
+
 #ifndef NUM_DIGITAL_PINS		// try harder... ?
   #warning "#define NUM_DIGITAL_PINS	// FIXME: ################"
 
