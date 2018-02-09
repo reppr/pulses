1fca9ec0e5044fe5ff632011c7078631fd288b5d
Robert Epprecht <epprecht@solnet.ch>
2018-02-09 17:13:02 +0000

    added some very basic ratio scales
    octaves
    octaves_fifths
    octaves_fourths
    octaves_fourths_fifths
    
    modified:   libraries/Pulses/examples/pulses/pulses.ino

--- libraries/Pulses/examples/pulses/pulses.ino
+++ libraries/Pulses/examples/pulses/pulses.ino
@@ -147,6 +147,10 @@ char * ratio_names[] = {
       "ratios_quot",		// 7
       "ratios_int",		// 8
       "ratios_rationals",	// 9
+      "octaves",		// 10
+      "octaves_fifths",		// 11
+      "octaves_fourths",	// 12
+      "octaves_fourths_fifths",	// 13
   };
 
   #define n_ratio_names (sizeof (ratio_names) / sizeof (const char *))
@@ -156,7 +160,11 @@ char * ratio_names[] = {
 // ratiotabs *MUST* have 2 trailing zeros
 
 
-unsigned int ratios_quot[] = {1,1, 1,2, 1,3, 1,4, 1,5, 1,6, 1,7, 1,8, 0,0};  // zero terminated
+unsigned int ratios_octaves[] = {1,1, 0,0};  				// zero terminated
+unsigned int ratios_octaves_fifths[] = {1,1, 2,3, 0,0};			// zero terminated
+unsigned int ratios_octaves_fourths[] = {1,1, 3,4, 0,0};		// zero terminated
+unsigned int ratios_octaves_fourths_fifths[] = {1,1, 3,4, 2,3, 0,0};	// zero terminated
+
 unsigned int ratios_int[]  = {1,1, 2,1, 3,1, 4,1, 5,1, 6,1, 7,1, 8,1, 0,0};  // zero terminated
 unsigned int ratios_rationals[]  = {1,1, 1,2, 2,3, 3,4, 5,6, 6,7, 7,8, 8,9, 9,10, 0,0};  // zero terminated
 
@@ -177,6 +185,7 @@ unsigned int major_scale[] = {1,1, 8,9, 4,5, 3,4, 2,3, 3,5, 8,15, 0,0};	// scale
 // 1:1	9:8	5:4	4:3	3:2	5:3	15:8	2:1
 
 unsigned int tetrachord[] = {1,1, 8,9, 4,5, 3,4, 0,0};			// scale each octave	zero terminated
+
 
 /* **************************************************************** */
 int selected_jiffle = ILLEGAL;
