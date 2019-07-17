e24c2c941706b5b1581cf03a93952933973c0303
Robert Epprecht <epprecht@solnet.ch>
2019-07-17 14:43:41 +0100

    lowered touch_threshold = 58
    maybe there *is* a value working ok with all my current instruments?
    
    TODO: possibility to calibrate touch level during startup
    
    modified:   pulses/touch.h
    modified:   pulses/infos/TODO.txt

--- pulses/touch.h
+++ pulses/touch.h
@@ -9,7 +9,9 @@
 #define DEBUG_TOUCH_INTERRUPT		// ################ TODO: remove debugging code
 
 gpio_pin_t test_PIN_touch = 4;		// == T0	// ################ TODO: remove debugging code
-int touch_threshold = 70;		// just a default...
+//int touch_threshold = 70;		// TODO: does not work well on some new instruments ???
+//int touch_threshold = 60;		// TODO: TEST&TRIM:
+int touch_threshold = 58;		// TODO: TEST&TRIM:
 #if defined DEBUG_TOUCH_INTERRUPT			// ################ TODO: remove debugging code
   long touch_cnt=0;
 #endif
